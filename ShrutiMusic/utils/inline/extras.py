43794cea00309ad54ca61568a5ac40a4ef44e3d2
SQBOT0 <samimsuperboy@gmail.com>
2026-07-29 20:11:38 +0530

    Update extras.py

--- ShrutiMusic/utils/inline/extras.py
+++ ShrutiMusic/utils/inline/extras.py
@@ -1,46 +1,32 @@
-# Copyright (c) 2025 Nand Yaduwanshi <NoxxOP>
-# Location: Supaul, Bihar
-#
-# All rights reserved.
-#
-# This code is the intellectual property of Nand Yaduwanshi.
-# You are not allowed to copy, modify, redistribute, or use this
-# code for commercial or personal projects without explicit permission.
-#
-# Allowed:
-# - Forking for personal learning
-# - Submitting improvements via pull requests
-#
-# Not Allowed:
-# - Claiming this code as your own
-# - Re-uploading without credit or permission
-# - Selling or using commercially
-#
-# Contact for permissions:
-# Email: badboy809075@gmail.com
-
-
-from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
-
+from ShrutiMusic.utils.inline.buttons import inline
 from config import SUPPORT_GROUP
 
 
 def botplaylist_markup(_):
     buttons = [
         [
-            InlineKeyboardButton(text=_["S_B_9"], url=SUPPORT_GROUP),
-            InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close"),
+            inline._button(
+                text=_["S_B_9"],
+                category="primary",
+                url=SUPPORT_GROUP
+            ),
+            inline._button(
+                text=_["CLOSE_BUTTON"],
+                category="danger",
+                callback_data="close"
+            ),
         ],
     ]
     return buttons
 
 
 def close_markup(_):
-    upl = InlineKeyboardMarkup(
+    upl = inline.ikm(
         [
             [
-                InlineKeyboardButton(
+                inline._button(
                     text=_["CLOSE_BUTTON"],
+                    category="danger",
                     callback_data="close",
                 ),
             ]
@@ -50,26 +36,15 @@ def close_markup(_):
 
 
 def supp_markup(_):
-    upl = InlineKeyboardMarkup(
+    upl = inline.ikm(
         [
             [
-                InlineKeyboardButton(
+                inline._button(
                     text=_["S_B_9"],
+                    category="primary",
                     url=SUPPORT_GROUP,
                 ),
             ]
         ]
     )
     return upl
-
-
-# ©️ Copyright Reserved - @NoxxOP  Nand Yaduwanshi
-
-# ===========================================
-# ©️ 2025 Nand Yaduwanshi (aka @NoxxOP)
-# 🔗 GitHub : https://github.com/NoxxOP/ShrutiMusic
-# 📢 Telegram Channel : https://t.me/ShrutiBots
-# ===========================================
-
-
-# ❤️ Love From ShrutiBots 
