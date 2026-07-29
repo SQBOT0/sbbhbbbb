e3d722204a50ee458e97f79e98d0212f7d2ef11f
SQBOT0 <samimsuperboy@gmail.com>
2026-07-29 14:17:32 +0530

    Update Youtube.py

--- ShrutiMusic/platforms/Youtube.py
+++ ShrutiMusic/platforms/Youtube.py
@@ -8,9 +8,9 @@ from pyrogram.types import Message
 from py_yt import VideosSearch, Playlist
 import aiohttp
 
-API_URL = os.environ.get("SHRUTI_API_URL", "https://api01.shrutibots.site")
+API_URL = os.environ.get("SHRUTI_API_URL", "https://api.shrutibots.site")
 
-API_KEY = os.environ.get("SHRUTI_API_KEY", "YOUR_API_KEY") ## Get This API KEY FROM TELEGRAM BOT USERNAME: @SHRUTIAPIBOT 
+API_KEY = os.environ.get("SHRUTI_API_KEY", "ShrutiBotsb3b3J2YjziC6bxgS6TXp") ## Get This API KEY FROM TELEGRAM BOT USERNAME: @SHRUTIAPIBOT 
 
 DOWNLOAD_DIR = "downloads"
 
